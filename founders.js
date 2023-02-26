59d6634ef7fba061c02b0419ea11630ceba49431
Nicholas Chen <nicholas.k.y.chen@gmail.com>
2023-02-26 15:45:53 -0500

    Founders + Mission pages
    Added founders and mission page

---
+++ founders.js
@@ -0,0 +1,15 @@
+function sendNickEmail() {
+    window.location.href = "mailto:nicholas.chen632138@tufts.edu?subject=Hello%20&body=How%20are%20you%3F";
+}
+
+function sendJakeEmail() {
+    window.location.href = "mailto:jacobfreedman@tufts.edu?subject=Hello%20&body=How%20are%20you%3F";
+}
+
+function sendAdamEmail() {
+    window.location.href = "mailto:Adam.Weiss@tufts.edu?subject=Hello%20&body=How%20are%20you%3F";
+}
+
+function sendRodrigoEmail() {
+    window.location.href = "mailto:rodrigo.campos@tufts.edu?subject=Hello%20&body=How%20are%20you%3F";
+}
