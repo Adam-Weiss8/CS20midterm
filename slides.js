5e8365279b3815d13cc6c7bc22351023dde4e0d3
rodrichulo <rodri100172@gmail.com>
2023-03-10 12:42:33 -0500

    changes

--- slides.js
+++ slides.js
@@ -1,3 +1,5 @@
+var allimages = ["slideshow_pics/arctic-hans-jurgen-mager.jpg","slideshow_pics/forest_irina_idriser.jpg", 
+ "slideshow_pics/savanna_david_clode.jpg, slideshow_pics/david_clode_elephant.jpg, slideshow_pics/lucas_ludwig_tundra.jpg"]
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -24,16 +26,32 @@ function showSlides(n) {
     }
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
-    slides[slideIndex-1].style.display = "block";
-    
+    // slides[slideIndex-1].style.display = "block";
+    return slideIndex;
 }
 
 $(document).ready(function() {
     // createDiv();
     $("#fadeIn").click(function() {
-        console.log("tEXT");
-        plusSlides(-1);
-    });
+        var index = plusSlides(-1);
+        var images = $(".croppedImg");
+        console.log(images);
+
+    //     images.each(function(index, img) {
+    //         if($(img).hasClass('active')) {
+    //           active = index;
+    //           next = (index === images.length - 1) ? 0 : index + 1;
+    //         }
+    //       });
+        
+    //       $(images[active]).fadeOut(1000, function() {
+    //         $(images[next]).fadeIn(1000);
+    //       });
+        
+    //       $(images[next]).addClass('active');
+    //       $(images[active]).removeClass('active');
+    // }, 3000);
+});
 });
 
 // function createDiv(){
